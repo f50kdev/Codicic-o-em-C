1054cc9856ddca30a8abe32f5d05dcf77948da4a
Faustino Henriques <71952910+f50kdev@users.noreply.github.com>
2023-01-09 23:34:48 +0000

    Add files via upload

---
+++ idade em anos.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+         char nome[50]; 
+         int nota1 , nota2,  media;
+
+         printf("Insira o seu nome\n\n");
+         scanf("%s" , &nome);
+
+         printf("Insira a sua primeira nota\n\n");
+         scanf("%d" , &nota1);
+
+         printf("Insira a sua segunda nota\n\n");
+         scanf("%d" , &nota2);
+
+         media = ( nota1 + nota2)/2;
+
+         if ( media >= 7 && media < 9){
+            printf("O senhor Vai ao Exame \n\n");
+
+       
+         
+
+         }         
+         
+    return 0;
+}
